418feb1a680098c372da1a7ac20501593a3fce35
Robson Peterson <robsonpeterson@hotmail.com>
2022-09-09 15:51:39 -0300

    Start

---
+++ src/main/java/com/dreamwork/gastemenos/GastemenosApplication.java
@@ -0,0 +1,13 @@
+package com.dreamwork.gastemenos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastemenosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastemenosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamwork/gastemenos/GastemenosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamwork.gastemenos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastemenosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
